12b3dae6f2292f6c2b09bf6319a818bdc8d8aabd
Jomario Santana <jomario.santanaan@elorrieta-errekamari.com>
2020-11-11 16:27:47 +0100

    ventana resumen

---
+++ CineElorrieta/src/main/java/CineElorrieta/VentanaResumen.java
@@ -0,0 +1,50 @@
+package CineElorrieta;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import java.awt.Font;
+
+public class VentanaResumen extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaResumen frame = new VentanaResumen();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaResumen() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("RESUMEN");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 40));
+		lblNewLabel.setBounds(10, 11, 210, 84);
+		contentPane.add(lblNewLabel);
+	}
+
+}
