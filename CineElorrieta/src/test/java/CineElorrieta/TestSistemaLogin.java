58bb4ca6633cd5b3ad3b07f2e580a6abeecd30e6
Jomario Santana <jomario.santanaan@elorrieta-errekamari.com>
2020-11-16 14:06:13 +0100

    testeo SistemaLogin

--- CineElorrieta/src/test/java/CineElorrieta/TestSistemaLogin.java
+++ CineElorrieta/src/test/java/CineElorrieta/TestSistemaLogin.java
@@ -1,5 +1,33 @@
 package CineElorrieta;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestSistemaLogin {
 
+	// Sistema.SistemaLogin sistemaLogin = new Sistema.SistemaLogin();
+	private String administrador="admin",password="1234";
+	private String adminEscrito,passEscrita;
+	private String resultado;
+	
+	@Test
+	public void testCheckAdmin() {
+		
+		adminEscrito="admin";
+		passEscrita="1234";
+		
+		if(adminEscrito.equals(administrador) && passEscrita.equals(password)) {	
+			//CORRECTO
+			resultado="Correcto";
+		 }else{
+			//INCORRECTO
+			resultado="Incorrecto"; 
+		 }     	
+		
+		assertEquals("Correcto", resultado);
+		
+	} 
+
 }
+
